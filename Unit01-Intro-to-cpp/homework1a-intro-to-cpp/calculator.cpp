4581ac6082b894bb032bd7dcc024659155a5867c
ifriedham <noah10101@live.com>
2023-09-24 18:41:28 -0600

    wip

--- Unit01-Intro-to-cpp/homework1a-intro-to-cpp/calculator.cpp
+++ Unit01-Intro-to-cpp/homework1a-intro-to-cpp/calculator.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 std::string input(std::string prompt) {
     std::cout << prompt;
@@ -10,7 +11,40 @@ std::string input(std::string prompt) {
 }
 
 int main(int argc, char const* argv[]) {
-    // Write your code here
+
+    string op;
+    cout << "operation: ";
+    cin >> op;
+
+    // add
+    if (op == "add") {
+        cout << "YAY ADD";
+    }
+
+    
+    //subtract
+    else if (op == "subtract"){
+        cout << "YAY SUBTRACT";
+    }
+
+
+    //multiply
+    else if (op == "subtract"){
+        cout << "YAY SUBTRACT";
+    }
+
+
+    //divide
+    else if (op == "subtract"){
+        cout << "YAY SUBTRACT";
+    }
+
+    
+    //mod (modulus)
+    else if (op == "subtract"){
+        cout << "YAY SUBTRACT";
+    }
+
 
     return 0;
 }
