4451437dbf7cd3d0b9fd5197779fd6dcf3774f09
ifriedham <noah10101@live.com>
2023-09-24 21:44:01 -0600

    hope this works?

--- Unit01-Intro-to-cpp/homework1a-intro-to-cpp/calculator.cpp
+++ Unit01-Intro-to-cpp/homework1a-intro-to-cpp/calculator.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
-std::string input(std::string prompt) {
+std::string input(std::string prompt)
+{
     std::cout << prompt;
     std::cout.flush();
     std::string response;
@@ -10,41 +10,61 @@ std::string input(std::string prompt) {
     return response;
 }
 
-int main(int argc, char const* argv[]) {
+int main(int argc, char const *argv[])
+{
+    // std::string operation;
+    int left, right;
 
-    string op;
-    cout << "operation: ";
-    cin >> op;
+    while (true)
+    {
+        std::string operation = input("operation: ");
 
-    // add
-    if (op == "add") {
-        cout << "YAY ADD";
+        if (operation.empty())
+        {
+            return 0;
+            //break;
+        }
+
+        else if (operation == "add" || operation == "minus" || operation == "multiply" || operation == "divide" || operation == "mod")
+        {
+            std::cout << "left operand: ";
+            std::string leftStr = input("");
+            std::cout << "right operand: ";
+            std::string rightStr = input("");
+
+            // Convert input strings to integers
+            left = std::stoi(leftStr);
+            right = std::stoi(rightStr);
+
+            if (operation == "add")
+            {
+                std::cout << left + right << std::endl;
+            }
+
+            else if (operation == "minus")
+            {
+                std::cout << left - right << std::endl;
+            }
+
+            else if (operation == "multiply")
+            {
+                std::cout << left * right << std::endl;
+            }
+
+            else if (operation == "divide")
+            {
+                std::cout << left / right << std::endl;
+            }
+
+            else if (operation == "mod")
+            {
+                std::cout << left % right << std::endl;
+            }
+        }
+
+        else
+        {
+            std::cout << operation << " isn't a valid operation" << std::endl;
+        }
     }
-
-    
-    //subtract
-    else if (op == "subtract"){
-        cout << "YAY SUBTRACT";
-    }
-
-
-    //multiply
-    else if (op == "subtract"){
-        cout << "YAY SUBTRACT";
-    }
-
-
-    //divide
-    else if (op == "subtract"){
-        cout << "YAY SUBTRACT";
-    }
-
-    
-    //mod (modulus)
-    else if (op == "subtract"){
-        cout << "YAY SUBTRACT";
-    }
-
-
-    return 0;
 }
