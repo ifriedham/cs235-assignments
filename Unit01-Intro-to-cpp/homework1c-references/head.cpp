b7fa99742e1f2f73887ce082f2179a24a611ee19
ifriedham <noah10101@live.com>
2023-10-05 17:46:55 -0600

    still don't understand why it's not working

--- Unit01-Intro-to-cpp/homework1c-references/head.cpp
+++ Unit01-Intro-to-cpp/homework1c-references/head.cpp
@@ -18,11 +18,11 @@ int main(int argc, char const* argv[]) {
     // Write your code here
     int n = 10;  // set default n
 
-    if (argc > 1) {  // if n is given, change from 10 to new number
+    /* if (argc > 2) {  // if n is given, change from 10 to new number
         n = atoi(argv[1]);
-    }
+    } */
 
-    if (argc > 2) {
+    if (argc == 3) {
         std::ifstream input(argv[2]);  // open file
 
         if (!input.is_open()) {  // input file doesn't exist
@@ -30,12 +30,36 @@ int main(int argc, char const* argv[]) {
             return 1;
 
         } else {  // file successfuly opened
+            n = atoi(argv[1]);
             printLines(n, input);
             input.close();
         }
 
-    } else {  // no file provided, printing from cin
-        printLines(n, std::cin);
+    } else if (argc == 2) {
+        std::ifstream input(argv[1]);  // open file
+
+        if (!input.is_open()) {       // input file doesn't exist
+            if (n = atoi(argv[1])) {  // should see if a line count is given?
+                printLines(n, std::cin);  // no file provided, printing from cin
+            } else {
+                std::cerr << "ERROR: can't open " << argv[2] << std::endl;
+                return 1;
+            }
+
+        } else {  // file successfuly opened
+            printLines(n, input);
+            input.close();
+        }
+    }
+
+    else if (argc == 1) {
+        printLines(n, std::cin);  // no file provided, printing from cin
+
+    }
+
+    else {
+        std::cout << "ERROR, NO ARGUMENTS GIVEN" << std::endl;
+        return 2;
     }
 
     return 0;
