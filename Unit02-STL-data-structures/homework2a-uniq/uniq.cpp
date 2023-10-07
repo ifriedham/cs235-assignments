c28ed06bce418da7408944ead0b2a425ee2d771b
ifriedham <noah10101@live.com>
2023-10-06 19:00:13 -0600

    made input.txt + started uniq

--- Unit02-STL-data-structures/homework2a-uniq/uniq.cpp
+++ Unit02-STL-data-structures/homework2a-uniq/uniq.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <list>
+using std::list;
 
-int main(int argc, char const* argv[]) {
+int main(int argc, char const *argv[]) {
     // Write your code here
+    list<std::string> rawLines;
+    std::string line;
+
+    std::ifstream input(argv[1]); //open input file
+
+    if (!input.is_open()) {  //if input is not a file
+        while (std::getline(std::cin, line)){
+            rawLines.push_back(line);
+        }
+    } else { //input is a file
+        while (std::getline(input, line)) {
+
+        }
+    }
+
 
     return 0;
 }
