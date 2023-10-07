0ea8d5e20a7d44c25f3f0fe804483c433b1c7edf
ifriedham <noah10101@live.com>
2023-10-06 20:33:29 -0600

    Uniq.cpp finished

--- Unit02-STL-data-structures/homework2a-uniq/uniq.cpp
+++ Unit02-STL-data-structures/homework2a-uniq/uniq.cpp
@@ -1,24 +1,37 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <list>
-using std::list;
+#include <vector>
+#include <unordered_set>
+
 
 int main(int argc, char const *argv[]) {
-    // Write your code here
-    list<std::string> rawLines;
+    std::vector<std::string> rawLines;
+    std::vector<std::string> finalLines;
+    std::unordered_set<std::string> uniqLines;
     std::string line;
 
     std::ifstream input(argv[1]); //open input file
 
     if (!input.is_open()) {  //if input is not a file
-        while (std::getline(std::cin, line)){
-            rawLines.push_back(line);
+        while (std::getline(std::cin, line)) {  //read cin
+            if (line.empty()) { break; } // blank line ends input
+            else { rawLines.push_back(line); }
         }
     } else { //input is a file
         while (std::getline(input, line)) {
-
+            rawLines.push_back(line);
         }
+    }
+
+    for (const std::string &nextLine: rawLines) {  //looking for duplicates
+        if (uniqLines.insert(nextLine).second) {
+            finalLines.push_back(nextLine);  //writing unique items to new vector
+        }
+    }
+
+    for (const std::string &nextLine: finalLines) {  // print out list
+        std::cout << nextLine << std::endl;
     }
 
 
