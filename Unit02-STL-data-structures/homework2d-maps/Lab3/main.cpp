7f4f63aecb581b1b021930454c7a2d7bafb3ce21
ifriedham <noah10101@live.com>
2023-10-21 17:12:27 -0600

    starting 2d

---
+++ Unit02-STL-data-structures/homework2d-maps/Lab3/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <sstream>
+#include <fstream>
+
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    vector <string> tokens;
+    set <string> unique;
+    string next_line;  // Each data line
+    string fileName = argv[1];
+    ifstream in(fileName); //opens file
+    while (getline(in, next_line)) {
+        istringstream iss(next_line);
+        string token;
+        while (iss >> token) {
+            string nopunct = "";
+            for (auto &c: token) { // Remove Punctuation
+                if (isalpha(c)) {
+                    nopunct += c;
+                }
+            }
+            tokens.push_back(nopunct);
+            unique.insert(nopunct);
+            // cout << token<<endl;
+        }
+    }
+    cout << "Number of words " << tokens.size() << endl;
+    cout << "Number of unique words " << unique.size() << endl;
+    ofstream setFile(fileName+"_set.txt");
+    for (set<string>::iterator it = unique.begin(); it != unique.end(); ++it) {
+        cout << ' ' << *it;
+        setFile << ' ' << *it;
+    }
+    cout << endl;
+}
