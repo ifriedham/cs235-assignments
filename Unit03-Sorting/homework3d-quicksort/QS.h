5b2a24ba9b08c0b8af6e67ff4b49313e8cd6f5f5
ifriedham <noah10101@live.com>
2023-10-26 23:16:33 -0600

    partition done (i think?)

--- Unit03-Sorting/homework3d-quicksort/QS.h
+++ Unit03-Sorting/homework3d-quicksort/QS.h
@@ -27,6 +27,34 @@ int medianOfThree(std::vector<T>& array, int left, int right) {
 template<class T>
 int partition(std::vector<T>& array, int left, int right) {
     // implement partition here
+    int middle = medianOfThree(array,left, right);
+
+    swap(array[left], array[middle]);  //pivot point is now array[left]
+
+    int up = left + 1;
+    int down = right;
+
+    while (up < down) {
+        while(array[up] <= array[left] || // until up points to the first element greater than the pivot value
+        up < right)  // until up has reached right
+        {
+            up++;
+        }
+
+        while(array[down] >= array[left] || // until down points to the first element less than or equal to the pivot value
+        down > left)  // until down has reached left
+        {
+            down--;
+        }
+
+        if (up < down){  // If up is still less than down, swap array[up] and array[down]
+            swap(array[up], array[down]);
+        }
+    }
+
+    swap(array[left], array[down]);  // Put the pivot back in the middle by swapping array[left] and array[down]
+
+    return down;
 }
 
 template<class T>
