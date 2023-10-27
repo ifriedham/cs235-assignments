ddce8fab11ee6d0a93f38737ed40c8689affa746
ifriedham <noah10101@live.com>
2023-10-26 23:01:19 -0600

    medianOfThree done

--- Unit03-Sorting/homework3d-quicksort/QS.h
+++ Unit03-Sorting/homework3d-quicksort/QS.h
@@ -2,9 +2,26 @@
 
 #include <vector>
 
+using std::swap;
+
 template<class T>
 int medianOfThree(std::vector<T>& array, int left, int right) {
     // implement medianOfThree here
+    int middle = (left + right) / 2;
+
+    if (array[left] > array[middle]) {
+        swap(array[left], array[middle]);
+    }
+
+    if (array[middle] > array[right]) {
+        swap(array[middle], array[right]);
+    }
+
+    if (array[left] > array[middle]) {
+        swap(array[left], array[middle]);
+    }
+
+    return middle;
 }
 
 template<class T>
