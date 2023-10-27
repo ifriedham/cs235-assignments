66397e205c0004f9978fa75a401d58d5eab94f66
ifriedham <noah10101@live.com>
2023-10-26 20:27:14 -0600

    maze initializing still WIP

--- Unit03-Sorting/homework3b-maze/maze.cpp
+++ Unit03-Sorting/homework3b-maze/maze.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 #include "Grid.h"
 
+using std::endl, std::cerr;
+
 int main(int argc, char* argv[]) {
     // Write your code here
+    std::ifstream inFile(argv[1]);  // open input file
+    if(!inFile){ // failed to open file
+        cerr << "ERROR: Unable to open input file!" << endl;
+        return 1;
+    }
+
+    // write maze from input file to newMaze
+    Grid newMaze;
+    inFile >> newMaze;
+    inFile.close();
+
+
+
+
+
+
+    //success!
+
+    //failed
+
 
     return 0;
 }
