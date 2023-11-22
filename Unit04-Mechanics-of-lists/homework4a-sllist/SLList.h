811d11d4256ed384509abdafb4901ccab7ea5d59
ifriedham <noah10101@live.com>
2023-11-22 16:40:51 -0700

    Working with tests, 4a done maybe?

--- Unit04-Mechanics-of-lists/homework4a-sllist/SLList.h
+++ Unit04-Mechanics-of-lists/homework4a-sllist/SLList.h
@@ -1,40 +1,86 @@
 #pragma once
 
-template <class T>
+template<class T>
 class SLList {
 public:
     struct Node {
-        Node* next;
+        Node *next;
         T value;
 
         Node(T v) : next(nullptr), value(v) {}
     };
 
-    SLList() {}
+    SLList() : head(nullptr), sllSize(0) {
+    }
 
-    ~SLList() {}
+    ~SLList() {
+        clear();
+    }
 
-    const Node* get_head() const {
+    const Node *get_head() const {
         // implement get_head here
+        return head;
     }
 
     void push_back(T item) {
         // implement push_back here
+        Node *newItem = new Node(item);
+
+
+
+       if (head) {
+           Node *nodePtr = head;
+           while (nodePtr != NULL) {
+               nodePtr = nodePtr->next; // move to the next node
+           }
+           nodePtr->next = newItem;
+           sllSize++; // = sllSize + 1;
+       }
+       else { // no existing head
+           head = newItem;
+       }
     }
 
     void pop_back() {
         // implement pop_back here
+        if (head->next) {
+            Node *nodePtr = head;
+            Node *prevNode = nullptr;
+            while (nodePtr->next) {
+                prevNode = nodePtr; // keep track of the previous node
+                nodePtr = nodePtr->next; // move to the next node
+            }
+            prevNode->next = nullptr;
+            delete nodePtr;
+        }
+        else { // Head is the only thing in the list
+            delete head;
+            head = nullptr;
+        }
+        sllSize--;
     }
 
-    const T& front() const {
+    const T &front() const {
         // implement front here
+        return head->value;
     }
 
     int size() const {
         // implement size here
+        return sllSize;
     }
 
     void clear() {
         // implement clear here
+        while (head) {
+            Node *delNode = head;
+            head = head->next;
+            delete delNode;
+            //sllSize--;
+        }
+        sllSize = 0;
     }
+private:
+    Node *head;
+    int sllSize;
 };
