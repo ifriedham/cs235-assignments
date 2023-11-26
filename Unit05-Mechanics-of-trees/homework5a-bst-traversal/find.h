c9dab80bb490b6c21a538e01a8c9af97df32fc04
ifriedham <noah10101@live.com>
2023-11-25 17:07:46 -0700

    5a done I think?

--- Unit05-Mechanics-of-trees/homework5a-bst-traversal/find.h
+++ Unit05-Mechanics-of-trees/homework5a-bst-traversal/find.h
@@ -1,14 +1,26 @@
 #pragma once
 
 struct Node {
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
     int value;
 
     Node(int v) : left(nullptr), right(nullptr), value(v) {}
 };
 
-bool find(Node* root, int item) {
+bool find(Node *root, int item) {
     // implement find here
     // return true if item is in the subtree, false otherwise
+    if (root == nullptr) {
+        return false;
+    }
+    if (root->value == item) {
+        return true;
+    }
+
+    if (item < root->value) { //check left
+        return find(root->left, item);
+    } else { // check right
+        return find(root->right, item);
+    }
 }
