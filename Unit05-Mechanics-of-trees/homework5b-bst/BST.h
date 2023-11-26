585d398a437ca434ebfa9a8ac6d4c1c4ba89ac00
ifriedham <noah10101@live.com>
2023-11-25 17:54:49 -0700

    5b first bit done (constructor / find / clear

--- Unit05-Mechanics-of-trees/homework5b-bst/BST.h
+++ Unit05-Mechanics-of-trees/homework5b-bst/BST.h
@@ -4,24 +4,28 @@ template<class T>
 class BST {
 public:
     struct Node {
-        Node* left;
-        Node* right;
+        Node *left;
+        Node *right;
         T value;
 
         Node(T v) : left(nullptr), right(nullptr), value(v) {}
     };
 
-    BST() {
-        // implement the contructor here
+    BST() : root(nullptr), nodeCount(0) {
+        // implement the constructor here
+//        root = nullptr;
+//        nodeCount = 0;
     }
 
     ~BST() {
         // implement the destructor here
+        clear();
     }
 
-    const Node* getRootNode() const {
+    const Node *getRootNode() const {
         // implement getRootNode here
         // return a pointer to the tree's root node
+        return root;
     }
 
     bool insert(T item) {
@@ -37,15 +41,44 @@ public:
     bool contains(T item) const {
         // implement contains here
         // return true if item is in the tree, false otherwise
+        find(root, item);
     }
 
     void clear() {
         // implement clear here
         // remove all nodes from the tree
+        clear(root);
+        root = nullptr;
+        nodeCount = 0;
     }
 
     int size() const {
         // implement size here
         // return the number of nodes in the tree
+        return nodeCount;
+    }
+
+private:
+    Node *root;
+    size_t nodeCount;
+
+    void clear(Node *&node) {
+        if (node == nullptr) {
+            return;
+        }
+        clear(node->left);
+        clear(node->right);
+        delete node;
+    }
+
+    bool find(Node *&node, T item) const {
+        if (node == nullptr) return false;
+        if (node->value == item) {
+            return true;
+        } else if (item < root->value) { //check left
+            return find(root->left, item);
+        } else { // check right
+            return find(root->right, item);
+        }
     }
 };
