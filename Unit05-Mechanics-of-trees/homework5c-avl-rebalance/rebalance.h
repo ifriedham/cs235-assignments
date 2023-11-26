cc786652c496d4f13b738beae31bd40edb07975b
ifriedham <noah10101@live.com>
2023-11-25 19:18:59 -0700

    promote left first draft

--- Unit05-Mechanics-of-trees/homework5c-avl-rebalance/rebalance.h
+++ Unit05-Mechanics-of-trees/homework5c-avl-rebalance/rebalance.h
@@ -11,9 +11,19 @@ struct Node {
 
 // Hint: you might find it helpful to write an update_height function that takes
 // a Node* and updates its height field based on the heights of its children
+void update_height (Node* node) {
+
+}
+
 
 void promote_left(Node*& root) {
     // implement promote_left here
+    auto new_root = root->right;
+    root->right = new_root->left;
+    new_root->left = root;
+    update_height(root);
+    update_height(new_root);
+    root = new_root;
 }
 
 void promote_right(Node*& root) {
