2770b422d94435d894de242be41b37b4db3d3b74
ifriedham <noah10101@live.com>
2023-12-05 16:52:31 -0700

    Hashset video stuff implimented, size() and contains() done too

--- Unit06-Mechanics-of-hashtables/homework6a-hashset/HashSet.h
+++ Unit06-Mechanics-of-hashtables/homework6a-hashset/HashSet.h
@@ -1,15 +1,43 @@
 #pragma once
 
+#include <list>
+#include <functional>
+
+using namespace std;
+
+const size_t INITIAL_CAPACITY = 5;
+const double MAX_FILL = 0.8;
+
 template<class T>
 class HashSet {
 public:
-    HashSet() {}
+    HashSet() {
+        setSize = 0;
+        capacity = INITIAL_CAPACITY;
+        data = new list<T>[INITIAL_CAPACITY];
+    }
 
-    ~HashSet() {}
+    ~HashSet() {
+        //clear();
+    }
 
     bool insert(T item) {
         // implement insert here
         // return true if item was inserted, false if item was already in the set
+        if (fillFactor() >= MAX_FILL) { // checks if set is too small
+            grow();
+        }
+
+        if (contains(item)) { // item is already in list
+            return false;
+        } else { // item not found, inserting as planned
+            size_t hashcode = hash<T>{}(item);
+            size_t index = hashcode % capacity;
+
+            data[index].push_front(item);
+            setSize++;
+            return true;
+        }
     }
 
     bool remove(T item) {
@@ -20,6 +48,16 @@ public:
     bool contains(T item) const {
         // implement contains here
         // return true if item is in the set, false otherwise
+        size_t hashcode = hash<T>{}(item);
+        size_t index = hashcode % capacity;
+        if (data[index].size() != 0) { // checks if list is empty
+            for (auto entry: data[index]) { // looks for item
+                if (entry == item) return false; // item found, insert not needed
+            }
+            return true; //item found!
+        } else { // list is empty
+            return false;
+        }
     }
 
     void clear() {
@@ -30,5 +68,30 @@ public:
     int size() const {
         // implement size here
         // return the number of elements in the set
+        return setSize;
+    }
+
+private:
+    size_t setSize;
+    size_t capacity;
+    list <T> *data;
+
+    double fillFactor() {
+        return static_cast<double>(setSize) / capacity;
+    }
+
+    void grow() {
+        size_t oldCapacity = capacity;
+        capacity = 2 * capacity + 1;
+        list <T> *oldData = data;
+        data = new list<T>[capacity];
+        setSize = 0;
+
+        for (size_t i = 0; i < oldCapacity; i++) { // copies old set to the new larger set
+            for (auto item: oldData[i]) {
+                insert(item);
+            }
+        }
+        delete[] oldData;
     }
 };
